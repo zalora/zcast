acffbbddd34cd0de6eb21d3771b5232337a78593
Wolfram Huesken <wolfram.huesken@zalora.com>
2016-06-27 11:26:09 +0800

    Now using logback, instead of logj, added default config to exclude connect/disconnect messages via Memcached client

--- src/main/java/com/zalora/zcast/Start.java
+++ src/main/java/com/zalora/zcast/Start.java
@@ -2,6 +2,7 @@ package com.zalora.zcast;
 
 import com.hazelcast.core.*;
 import com.hazelcast.logging.*;
+import com.hazelcast.config.Config;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.zalora.zcast.interceptor.CompressionInterceptor;
@@ -22,7 +23,10 @@ public class Start {
             .omitEmptyStrings()
             .split(System.getProperty("compressed.maps", ""));
 
-        final HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        final Config config = new Config()
+            .setProperty("hazelcast.logging.type", "slf4j");
+
+        final HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
         final LoggingService loggingService = hz.getLoggingService();
         final ILogger logger = loggingService.getLogger(Start.class.getClass());
         final CompressionInterceptor compressionInterceptor = new CompressionInterceptor();
