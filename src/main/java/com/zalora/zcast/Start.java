01006e8dd307d009ae917d11ea8f880628ea073e
Wolfram Huesken <wolfram.huesken@zalora.com>
2016-06-27 17:13:22 +0800

    Moved maps and log path to hazelcast.xml

--- src/main/java/com/zalora/zcast/Start.java
+++ src/main/java/com/zalora/zcast/Start.java
@@ -1,6 +1,7 @@
 package com.zalora.zcast;
 
 import com.hazelcast.core.*;
+import com.hazelcast.config.*;
 import com.hazelcast.logging.*;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
@@ -11,29 +12,45 @@ import com.zalora.zcast.interceptor.CompressionInterceptor;
  */
 public class Start {
 
+    private static final String BUNYAN_DEFAULT_LOG_PATH = "zcast.json";
+    private static final CompressionInterceptor COMPRESSION_INTERCEPTOR = new CompressionInterceptor();
+
     /**
      * Get list of maps to compress from cli
      * java -Dcompressed.maps=hz_memcache_map1,hz_memcache_map2
      * @param args Should be empty...
      */
     public static void main(String[] args) {
-        Iterable<String> mapNames = Splitter.on(',')
-            .trimResults()
-            .omitEmptyStrings()
-            .split(System.getProperty("compressed.maps", ""));
+        Config config = new XmlConfigBuilder().build();
+        setupLogPath(config);
 
-        final HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        final HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
         final LoggingService loggingService = hz.getLoggingService();
         final ILogger logger = loggingService.getLogger(Start.class.getClass());
-        final CompressionInterceptor compressionInterceptor = new CompressionInterceptor();
 
+        Iterable<String> mapNames = getCompressedMaps(config);
         for (String mapName : mapNames) {
-            hz.getMap(mapName).addInterceptor(compressionInterceptor);
+            hz.getMap(mapName).addInterceptor(COMPRESSION_INTERCEPTOR);
             logger.info(String.format("Added Compression Interceptor to %s", mapName));
         }
 
         if (Iterables.size(mapNames) == 0) {
             logger.severe("Launching ZCast without compression!");
         }
+    }
+
+    private static Iterable<String> getCompressedMaps(Config config) {
+        return Splitter.on(',')
+            .trimResults()
+            .omitEmptyStrings()
+            .split(config.getProperty("zcast.maps"));
+    }
+
+    private static void setupLogPath(Config config) {
+        String logPath = config.getProperty("zcast.logging.file");
+        if (logPath == null) {
+            logPath = BUNYAN_DEFAULT_LOG_PATH;
+        }
+        System.setProperty("zcast.logging.file", logPath);
     }
 }
